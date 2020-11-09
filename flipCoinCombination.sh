34ae5bd5a5d8fc4e66535068b855eb2cec99cc42
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 18:57:09 +0530

    Refactor : Upgraded code to Doublet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,20 +4,22 @@ flipcount=50
 declare -A flip_log
 while [ $flipcount -gt 0 ] ; do
 	((flip1=RANDOM%2))
-	#((filp2=RANDOM%2))
-	case $flip1 in
-	0)
-		((flip_log[H_count]+=1))
+	((flip2=RANDOM%2))
+	case $flip1$flip2 in
+	00)
+		((flip_log[HH_count]+=1))
 		;;
-	1)
-                ((flip_log[T_count]+=1))
+	01)
+                ((flip_log[HT_count]+=1))
                 ;;
 
-        #        ((th_count+=1))
-        #        ;;
-	#11)
-        #        ((tt_count+=1))
-        #        ;;
+        10)
+                ((flip_log[TH_count]+=1))
+                ;;
+        11)
+                ((flip_log[TT_count]+=1))
+                ;;
+
 	esac
 	((flipcount-=1))
 done
