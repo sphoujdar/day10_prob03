cfd187db9f92a8a1e83ee61ee9619e7cb96b0082
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 18:48:40 +0530

    Add : Multiple Flips and storing their log in Dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,32 @@
 #!/bin/bash -x
 
+flipcount=50
+declare -A flip_log
+while [ $flipcount -gt 0 ] ; do
+	((flip1=RANDOM%2))
+	#((filp2=RANDOM%2))
+	case $flip1 in
+	0)
+		((flip_log[H_count]+=1))
+		;;
+	1)
+                ((flip_log[T_count]+=1))
+                ;;
 
-((flip=RANDOM%2))
+        #        ((th_count+=1))
+        #        ;;
+	#11)
+        #        ((tt_count+=1))
+        #        ;;
+	esac
+	((flipcount-=1))
+done
 
-if [ $flip -eq 0 ] ; then
-	echo Heads
-else
-	echo Tails
-fi
+
+for key in ${!flip_log[@]}  ; do
+	echo $key --- ${flip_log[$key]}
+	echo Percentage of $key is $(($((${flip_log[$key]}*100))/50))\%
+done
+
+
+
